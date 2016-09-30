c76c7d8b6f2a52ccdab93fa2eea212689fa51730
Stan Lin <ttttt123465@gmail.com>
2016-09-30 22:03:57 +0800

    Modify player scripts

---
+++ Survivor/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Image cursor;
+
+	private Rigidbody rb;
+	private Animator animator;
+	private float speed = 15f;
+	private Vector3 movement;
+
+	PlayerShooting a;
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+		animator = GetComponentInChildren<Animator> ();
+		Cursor.visible = false;
+	}
+
+	void Update() {
+		if (Time.timeScale!=0) {
+			Cursor.visible = false;
+			cursor.transform.position = Input.mousePosition;
+			cursor.enabled = true;
+		} else {
+			Cursor.visible = true;
+			cursor.enabled = false;
+		}
+	}
+
+	void FixedUpdate(){
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+		Move (vertical, horizontal);
+		Turn ();
+	}
+
+	void Move(float v,float h){
+		movement.Set (h, 0f, v);
+		rb.velocity = movement*speed;
+
+		if (rb.velocity.magnitude == 0f && animator.GetBool ("Walking") == true) {
+			animator.SetBool ("Walking", false);
+		} else if (rb.velocity.magnitude != 0f && animator.GetBool ("Walking") == false) {
+			animator.SetBool ("Walking", true);
+		}
+	}
+
+	void Turn(){
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		// Create a vector from the player to the point on the floor the raycast from the mouse hit.
+		Vector3 playerToMouse = mousePos - transform.position;
+
+		// Ensure the vector is entirely along the floor plane.
+		playerToMouse.y = 0f;
+
+		// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+		Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+
+		// Set the player's rotation to this new rotation.
+		rb.MoveRotation (newRotation);
+	}
+
+}
---
+++ Survivor/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public GameObject bullet;
+	Dictionary<int, Weapon> weapons = new Dictionary<int, Weapon> ();
+
+	//Texts
+	public Text pistolBullets;
+	public Text clipStatus;
+
+	//AudioClips
+	public AudioClip knifeSound;
+	public AudioClip pistolSound;
+
+	//Weapon Positions
+	public Transform pistolPos;
+
+	//Weapons
+	public Weapon knife;
+	public Weapon pistol;
+	public Weapon currentWeapon;
+
+	private Animator animator;
+	private AudioSource audiosource;
+	private CapsuleCollider knifeRange;
+	private int weaponNumber = 2;
+	private float timer = 0f;
+
+	void Awake() {
+		animator = GetComponentInChildren<Animator> ();
+		audiosource = GetComponent<AudioSource> ();
+		knifeRange = GetComponent<CapsuleCollider> ();
+
+		knife = new Weapon(0, 0, 0, 0.25f, knifeSound);
+		pistol = new Weapon (22, 7, 7, 0.25f, pistolSound);
+	}
+
+	void Start () {
+		AddWeapons ();
+		currentWeapon = weapons [2];
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+		if (Input.GetButton("Fire1") && timer >= currentWeapon.rate) {
+			Attack ();
+		}
+		if (timer >= 0.1f && knifeRange.enabled == true) {
+			knifeRange.enabled = false;
+		}
+	}
+
+	public class Weapon {
+		public int ammo;
+		public int clipSize;
+		public int currentClip;
+		public float rate;
+		public AudioClip sound;
+
+		public Weapon (int _ammo, int _clipSize, int _currentClip, float _rate, AudioClip _sound) {
+			ammo = _ammo;
+			clipSize = _clipSize;
+			currentClip = _currentClip;
+			rate = _rate;
+			sound = _sound;
+		}
+	}
+
+	void Attack () {
+		timer = 0f;
+
+		if (weaponNumber == 1) {
+			knifeRange.enabled = true;
+			audiosource.Play ();
+			animator.SetTrigger ("Attack");
+		} else {
+			if (currentWeapon.currentClip > 0) {
+				audiosource.Play ();
+				animator.SetTrigger ("Attack");
+				currentWeapon.currentClip--;
+				clipStatus.text = currentWeapon.currentClip + " / " + currentWeapon.clipSize;
+
+				switch (weaponNumber) {
+				case 2:
+					Instantiate (bullet, pistolPos.position, pistolPos.rotation);
+					break;
+				}
+			}
+		}
+	}
+
+	void AddWeapons () {
+		weapons.Add (1, knife);
+		weapons.Add (2, pistol);
+	}
+
+}
